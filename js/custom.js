a6e1294f9ab114ed79e84bd383a83c2c275ef8d7
Supun Kandaudahewa <skandaudahewa@gmail.com>
2020-11-28 01:03:03 +0530

    final27_11

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,29 @@
 const frm = document.getElementById("addbooks");
+let unamedefault = "user";
+let pworddefault = "pass";
 
-function validateUser(frm) {
-  var uname = frm.username.value;
-  var pword = frm.password.value;
+function validateUser() {
+  var uname = document.getElementById("username").value;
+  var pword = document.getElementById("password").value;
 
   var msg = document.getElementById("errormsg");
 
   if (uname.length == 0) {
     username.focus();
-    event.preventDefault();
+    document.getElementById("username").style.border = "3px solid red";
     return false;
   } else if (pword.length == 0) {
     pname.focus();
-    event.preventDefault();
+    document.getElementById("password").style.border = "3px solid red";
     return false;
   } else if (uname == "user" || pword == "pass") {
-    document.write("Authenticated");
+    document.getElementById("login").value = "Log Out";
+    document.getElementById("errormsg").style.color = "green";
+    document.getElementById("errormsg").innerHTML =
+      "Logged in, Log out when you are done";
     return true;
+    event.preventDefault();
   } else {
-    document.write("Authentication Failed");
     return false;
   }
 }
