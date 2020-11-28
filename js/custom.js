0fee5e86f1dc39d4aee41ef62d9e59459b3188e6
Supun Kandaudahewa <skandaudahewa@gmail.com>
2020-11-28 15:23:21 +0530

    addbooks_complete

--- js/custom.js
+++ js/custom.js
@@ -1,51 +1,63 @@
-const frm = document.getElementById("addbooks");
-let unamedefault = "user";
-let pworddefault = "pass";
+// Script to validate the user
 
-function validateUser() {
-  var uname = document.getElementById("username").value;
-  var pword = document.getElementById("password").value;
-
+function loginUser() {
+  var uname = this.document.getElementById("username").value.trim();
+  var pword = this.document.getElementById("password").value.trim();
   var msg = document.getElementById("errormsg");
+  var status = this.document.getElementById("login").value;
 
-  if (uname.length == 0) {
-    username.focus();
-    document.getElementById("username").style.border = "3px solid red";
-    return false;
-  } else if (pword.length == 0) {
-    pname.focus();
-    document.getElementById("password").style.border = "3px solid red";
-    return false;
-  } else if (uname == "user" || pword == "pass") {
-    document.getElementById("login").value = "Log Out";
-    document.getElementById("errormsg").style.color = "green";
+  if (status == "Log Out") {
+    //Log Out Procedure
+    this.document.getElementById("login").value = "Log In";
     document.getElementById("errormsg").innerHTML =
-      "Logged in, Log out when you are done";
-    return true;
-    event.preventDefault();
-  } else {
-    return false;
+      "You are not logged in currently. log in to continue!!!";
+    document.getElementById("errormsg").style.color = "#fff";
+    document.getElementById("username").disabled = false;
+    document.getElementById("password").disabled = false;
+
+    // Disabling Form
+    document.getElementById("dataForm").disabled = true;
+  } else if (status == "Log In") {
+    //Check for null values
+    if (uname == "" && pword == "") {
+      document.getElementById("username").style.border = "1px solid red";
+      document.getElementById("password").style.border = "1px solid red";
+      document.getElementById("errormsg").innerHTML =
+        "Enter Username and Password to proceed";
+      return false;
+    } else if (pword == "") {
+      document.getElementById("password").style.border = "1px solid red";
+      document.getElementById("username").style.border = "none";
+      document.getElementById("errormsg").innerHTML = "Password cant be blank";
+      return false;
+    } else if (uname == "") {
+      document.getElementById("username").style.border = "1px solid red";
+      document.getElementById("password").style.border = "none";
+      document.getElementById("errormsg").innerHTML = "Username cant be blank";
+      return false;
+    } else if (uname == "user" && pword == "pass") {
+      document.getElementById("login").value = "Log Out"; // Successfully Logged in
+      document.getElementById("username").style.border = "none";
+      document.getElementById("password").style.border = "none";
+      document.getElementById("errormsg").style.color = "#ffc107";
+      document.getElementById("errormsg").innerHTML =
+        "Logged in, Log out when you are done";
+      document.getElementById("username").disabled = true;
+      document.getElementById("password").disabled = true;
+
+      // Clear username and password fields
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+
+      // Enaabling Form
+      document.getElementById("dataForm").disabled = false;
+
+      return true;
+    } else {
+      document.getElementById("errormsg").innerHTML =
+        "Invalid Username/Password";
+      document.getElementById("username").style.border = "1px solid red";
+      document.getElementById("password").style.border = "1px solid red";
+    }
   }
 }
-
-// function validateUser(frm) {
-//   const uname = frm.username.value.trim();
-//   const pword = frm.password.value.trim();
-
-//   /* Check if text lenths are zero or not*/
-//   if (uname.length == 0 || pword.length == 0) {
-//     return false;
-//   }
-//   /* */
-
-//   /* */
-
-//   if (uname == "user" || pword == "pass") {
-//     window.location.replace("./index.html");
-//     document.write("Authenticated");
-//     return true;
-//   } else {
-//     document.write("Authentication Failed");
-//     return false;
-//   }
-// }
